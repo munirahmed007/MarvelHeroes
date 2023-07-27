38ed7f914430a0d59a449137a597eaa2046549e2
Munir Ahmed <mnirahd@gmail.com>
2023-07-27 05:15:33 +0100

    first push to repo

---
+++ MarvelComicHeroes/MarvelComicHeroes/AppDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  AppDelegate.swift
+//  MarvelComicHeroes
+//
+//  Created by Munir Ahmed on 27/07/2023.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+/*
+ 
+ 0c037a86592b89a57cb59ce222fcc14e
+ Your private key
+ 817c4627fbdb6d150a39d91379b474699f51eb9c
+ */
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ MarvelComicHeroes/MarvelComicHeroes/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MarvelComicHeroes
+//
+//  Created by Munir Ahmed on 27/07/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
